566ee6dfa462a6e5afc381758e917d2343824559
Avishek Amin <avishekamin207@gmail.com>
2026-06-06 00:48:27 +0530

    Added StarCross, StarKyte, HollowStar - Square, Rectangle, Rhombos and Parallelogram pattern problems

---
+++ Patterns/HollowStarRectangle.java
@@ -0,0 +1,28 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class HollowStarRectangle {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int row = sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int column = sc.nextInt();
+        sc.close();
+
+        System.out.println("Pattern: ");
+        for(int i=1; i<=row; i++) {
+            for(int j=1; j<=column; j++) {
+                if(i == 1 || i == row || j == 1 || j == column) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Patterns/StarKyte.java
@@ -0,0 +1,52 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class StarKyte {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        System.out.println("Pattern: ");
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i; j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++) {
+                System.out.print("* ");
+            }
+            for(int j=1; j<=i-1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=2; i<=n; i++) {
+            for(int j=1; j<=i-1; j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=n-i+1; j++) {
+                System.out.print("* ");
+            }
+            for(int j=1; j<=n-i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=1; i<=n-2; i++) {
+            for(int j=1; j<=n-i; j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++) {
+                System.out.print("* ");
+            }
+            for(int j=1; j<=i-1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
