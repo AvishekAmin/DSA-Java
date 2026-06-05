474ed8a4ac6b36ff389b4dab00f0df9e2f2d0650
Avishek Amin <avishekamin207@gmail.com>
2026-06-06 01:34:23 +0530

    Added Hollow programs for StarKyte, StarTriangle, StarTriangleMirror, StarTriangleReverse and StarTriangleReverseMirror programs

---
+++ Patterns/HollowStarTriangle.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class HollowStarTriangle {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        System.out.println("Pattern: ");
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                if(j == 1 || i == n || i == j) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
