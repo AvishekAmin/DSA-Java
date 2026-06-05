3585e5ae3e18c7cc267900fa6907ca1dc0f3eaf9
Avishek Amin <avishekamin207@gmail.com>
2026-06-06 02:40:53 +0530

    Added Hollow program codes for StarButterfly, StarDiamond, StarTable and StarTableReverse

---
+++ Patterns/HollowStarButterfly.java
@@ -0,0 +1,62 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class HollowStarButterfly {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        System.out.println("Pattern: ");
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                if(j == 1 || i == j) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            for(int j=1; j<=n-i; j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=n-i; j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++) {
+                if(j == 1 || i == j) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i+1; j++) {
+                if(j == 1 || j == n-i+1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            for(int j=1; j<=i-1; j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i-1; j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=n-i+1; j++) {
+                if(j == 1 || j == n-i+1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
