22a4d145aefa7e526f2ed14ffbf037855047426b
Avishek Amin <avishekamin207@gmail.com>
2026-06-05 15:34:33 +0530

    Added Star Triangle, Star Triangle Mirror, Reverse and Reverse-Mirror, Square Star and Rectangle Star Patterns

---
+++ Patterns/TriangleStarMirror.java
@@ -0,0 +1,25 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class TriangleStarMirror {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        System.out.println("Pattern: ");
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i; j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
