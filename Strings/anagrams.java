c2387a1256b27e79e34a2c98fd14d6b15092709e
Avishek Amin <avishekamin207@gmail.com>
2026-08-05 04:57:03 +0530

    Added java programs to count total number of vowels and to check whether 2 strings are anagrams of each other or not

---
+++ Strings/anagrams.java
@@ -0,0 +1,53 @@
+package Strings;
+
+import java.util.*;
+
+public class anagrams {
+    public static boolean checkAnagrams(String str1, String str2) {
+
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        int charStr1 = str1.length();
+        int charStr2 = str2.length();
+        int count = 0;
+
+        if (charStr1 != charStr2) {
+            return false;
+        } else {
+            boolean visited[] = new boolean[str1.length()];
+
+            for(int i = 0; i < str1.length(); i++) {
+                for(int j = 0; j < str2.length(); j++) {
+
+                    if(!visited[j] && str1.charAt(i) == str2.charAt(j)) {
+                        visited[j] = true;
+                        count++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if(count == str1.length()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 1st string: ");
+        String str1 = sc.nextLine();
+        System.out.print("Enter 2nd string: ");
+        String str2 = sc.nextLine();
+        sc.close();
+
+        if(checkAnagrams(str1, str2) == true) {
+            System.out.print("Strings are anagrams of each other");
+        } else {
+            System.out.print("Strings are not anagrams of each other");
+        }
+    }
+}
---
+++ Strings/anagrams2.java
@@ -0,0 +1,43 @@
+package Strings;
+
+import java.util.*;
+
+public class anagrams2 {
+    public static boolean checkAnagrams(String str1, String str2) {
+
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        int charStr1 = str1.length();
+        int charStr2 = str2.length();
+        int count = 0;
+
+        if (charStr1 != charStr2) {
+            return false;
+        } else {
+            char[] str1CharArray = str1.toCharArray();
+            char[] str2CharArray = str2.toCharArray();
+
+            Arrays.sort(str1CharArray);
+            Arrays.sort(str2CharArray);
+
+            boolean result = Arrays.equals(str1CharArray, str2CharArray);
+            return result;
+        }
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 1st string: ");
+        String str1 = sc.nextLine();
+        System.out.print("Enter 2nd string: ");
+        String str2 = sc.nextLine();
+        sc.close();
+
+        if(checkAnagrams(str1, str2) == true) {
+            System.out.print("Strings are anagrams of each other");
+        } else {
+            System.out.print("Strings are not anagrams of each other");
+        }
+    }
+}
